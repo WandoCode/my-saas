30d8dcf4691ba7ad570f5200bf8a2d71f34a3100
Maxime Chirez <mchirez@gmail.com>
2023-10-08 21:22:25 +0200

    Add style for main heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main>aaaaaaaa</main>
+  return (
+    <main>
+      <div className="container center">
+        <h1 className="heading">
+          Exemple d'un site web avec le branding de&nbsp;
+          <span className="fc-primary-600">Fleximed</span>
+        </h1>
+      </div>
+    </main>
+  )
 }
