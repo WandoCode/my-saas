e346b3477f602a6d71ffb2114d151ef432add6fd
Maxime Chirez <mchirez@gmail.com>
2023-10-09 21:10:37 +0200

    Add content to hero section with basic style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,27 @@ export default function Home() {
     <main>
       <div className="container center">
         <section className="hero">
-          <h1 className="heading">
-            Exemple d'un site web avec le branding de&nbsp;
-            <span className="fc-primary-600">Fleximed</span>
-          </h1>
-          <Button as="next-link" variant="primary">
-            Découvrir
-          </Button>
+          <div className="hero__container center">
+            <h1 className="heading h1">
+              Exemple d'un site web avec le branding de&nbsp;
+              <span className="fc-primary-600">Fleximed</span>
+            </h1>
+            <h2 className="heading subtitle">
+              Find easily healthcare worker you can trust!
+            </h2>
+            <div className="align hero__ctas">
+              <Button as="next-link" variant="primary" className="hero__btn-a">
+                Découvrir
+              </Button>
+              <Button
+                as="next-link"
+                variant="secondary"
+                className="hero__btn-a"
+              >
+                Schedule a demo
+              </Button>
+            </div>
+          </div>
         </section>
       </div>
     </main>
