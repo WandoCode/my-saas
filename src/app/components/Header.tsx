8ec09796020536921a5ba5765ae9e069c61b25b1
Maxime Chirez <mchirez@gmail.com>
2023-10-10 22:48:01 +0200

    Header style change when the screen is not on top of the page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,26 @@
+'use client'
+
 import Image from 'next/image'
 import { Nav } from '.'
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
 
 function Header() {
+  const [isTop, setIsTop] = useState<boolean>(true)
+
+  const handleScroll = () => {
+    if (window.scrollY < 100) setIsTop(true)
+    else if (window.scrollY >= 100 && isTop) setIsTop(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll)
+
+    return () => document.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header className="header">
+    <header className={clsx('header', { 'header--scroll': !isTop })}>
       <div className="header__logo-container">
         <Image
           className="header__logo"
