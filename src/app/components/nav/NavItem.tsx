31df92b37038750720bf08dfe02ed6e2454a9aa4
Maxime Chirez <mchirez@gmail.com>
2023-10-11 22:47:02 +0200

    Add tabulation navigation

--- src/app/components/nav/NavItem.tsx
+++ src/app/components/nav/NavItem.tsx
@@ -1,7 +1,14 @@
+'use client'
 import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-import { PropsWithChildren, useState } from 'react'
+import React, {
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+  useRef,
+  useState,
+} from 'react'
 
 interface PropsNavItem extends PropsWithChildren {
   href: string
@@ -19,14 +26,30 @@ function NavItem({
   toggleSubmenu,
   children,
 }: PropsNavItem) {
+  const itemRef = useRef<HTMLLIElement>(null)
+  const [focused, setFocused] = useState<boolean>(false)
+
+  const showSubmenu = () => setFocused(true)
+
+  const handleBlur = (e: React.FocusEvent) => {
+    const nextTarget = e.relatedTarget
+
+    if (!itemRef.current?.contains(nextTarget)) setFocused(false)
+  }
+
   return (
     <li
+      ref={itemRef}
       className={clsx(`${baseClassName}__item align`, {
         'submenu-open': submenuIsOpen,
+        'menu-focused': focused,
       })}
+      onFocus={showSubmenu}
+      onBlur={handleBlur}
     >
-      <Link href={href} className="only-desktop align">
+      <Link href={href} className="only-desktop align" tabIndex={0}>
         {children}
+
         {subItems && (
           <Image
             className={`${baseClassName}__expand-img`}
